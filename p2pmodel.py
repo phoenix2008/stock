49897954653c8e9e702108706f1a3212a1482c9b
zhang,gui <tech_a@163.com>
2019-01-08 21:22:00 +0800

    20190108

--- p2pmodel.py
+++ p2pmodel.py
@@ -7,19 +7,29 @@ from optparse import OptionParser
 
 logger = utils.setup_logger(name='p2pmodel')
 
+'''
+参数：
+初始利润，年增长率，
+用户数，每用户借款额，每用户借款的take rate
+'''
+
 def valuebyprofit():
     # 1, 最简单的按2018年利润，3年复合增长计算
-    value_bad  = utils.complex(3.5, 0.1, 5)
-    print value_bad*3
+    value_good = utils.complex(3.5, 0.1, 5)
+    logger.info('2023年利润-按2018季度利润计算 %s 亿' % float(value_good))
+    logger.info('市值按利润计算 %s 亿' % float(value_good*5))
+
 
 def valuebyusers():
     # 2, 按用户数计算
-    #拍拍贷财报显示200万用户数
-    value_good = utils.complex(200,0.05,3)
+    #拍拍贷财报显示320万单一活跃借款用户数 2018 q3, 年估计有1000万
+    value_good = utils.complex(1000,0.05,5)/10000
     #每用户价值=平均用户借款额*5%（利润）= 4000*5%
-    logger.info( '本年利润-按用户数计算 %s' % float(200*4000*0.05))
+    value_per_user = float(4000*0.05)
+    result_usd = float(value_good*value_per_user)/7
+    logger.info('2023本年利润-按用户数计算 %s 亿' % result_usd)
 
-    logger.info(value_good*4000*0.05)
+    logger.info('按用户数计算市值，5倍年利率，市值：%s 亿' % float(result_usd*5) )
 
 
 def getOpt():
